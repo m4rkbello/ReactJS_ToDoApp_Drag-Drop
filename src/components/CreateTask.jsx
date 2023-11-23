c1ae148eb5962c639f88914ae9f97940c2500528
purge710 <johnrussel338@gmail.com>
2023-11-23 23:46:03 +0800

    unfinished LocalStorage Parcing!

--- src/components/CreateTask.jsx
+++ src/components/CreateTask.jsx
@@ -1,33 +1,37 @@
 import { useState } from "react";
 import { v4 as uuidv4 } from 'uuid';
 
-const reateTask = ({tasks, setTasks}) => {
+const CreateTask = ({ tasks, setTasks }) => {
+
    const [task, setTask] = useState({
         id: "",
         name: "",
-        status: "TODO", //also be inprogress or closed!
+        status: "TODO", // also be in progress or closed!
    });
-   console.log(task);
 
-   const handleSubmit = (e) => {
-        e.preventDefault();
+//    useEffect(() => {
+//     console.log("Component mounted or updated");
+//  }, [tasks]); // Log when tasks change
 
-    setTasks = ((prev) => {
-        const List = [...prev, task];
-        localStorage.setItem("tasks", JSON.stringify(List));
-        return List;
+
+ const handleSubmit = (e) => {
+    e.preventDefault();
+    //state properties
+    setTasks((prev) => {
+        const newList = [...prev, task];
+        localStorage.setItem("tasks", JSON.stringify(newList));
+        return newList;
     });
-   };
+};
     
-   
     return ( 
         <form onSubmit={handleSubmit}>
-            <input 
-            type="text"
-             className="border-2 border-slate-400 bg-slate-100 rounded-md mr-4 h-12 w-64 px-1"
-             value={task.name} 
-             onChange={(e) => setTask( {...task, id: uuidv4(), name: e.target.value })}
-             />
+            <input
+                type="text"
+                className="border-2 border-slate-400 bg-slate-100 rounded-md mr-4 h-12 w-64 px-1"
+                value={task.name} 
+                onChange={(e) => setTask({ ...task, id: uuidv4(), name: e.target.value })}
+            />
             <button className="bg-cyan-500 rounded-md px-4 h-12 text-white">Create</button>
         </form>
      );
